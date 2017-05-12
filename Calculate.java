1e70199d8c901243831c3d416e5de40455cf96ba
Tachkin <TachDit@yandex.ru>
2017-05-13 01:33:21 +0300

    Lesson edit Calculate class

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,8 @@
 public class Calculate{
 	public static void main (String[] args){
-		System.out.println("Hello world");
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[0]);
+		int sum = first + second;
+		System.out.println("sum = " + sum);
 	}
 }
