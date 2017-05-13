c65493927d6eb8ecd664cbce6c789c060931c36d
Tachkin <TachDit@yandex.ru>
2017-05-13 07:33:04 +0300

    Lesson 3 home work

--- Calculate.java
+++ Calculate.java
@@ -1,8 +1,24 @@
+import java.lang.Math;
+
 public class Calculate{
 	public static void main (String[] args){
-		int first = Integer.valueOf(args[0]);
-		int second = Integer.valueOf(args[0]);
+		String action = args[0];
+		int first = Integer.valueOf(args[1]);
+		int second = Integer.valueOf(args[2]);
+		int result = 0;
+		switch (action){
+			case "sum": result = first + second;
+				break;
+			case "sub": result = first - second;
+				break;
+			case "div": result = first / second;
+				break;
+			case "mult": result = first * second;
+				break;
+			case "pow": result = (int)Math.pow(first , second);
+				break;
+		}
 		int sum = first + second;
-		System.out.println("sum = " + sum);
+		System.out.println("result = " + result);
 	}
 }
